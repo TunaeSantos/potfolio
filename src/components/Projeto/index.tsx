1d7a770641663d696aa2d160d9b0a141d4af42f0
Tunae <tunae.santos82@gmail.com>
2024-08-19 14:06:50 -0300

    Criando a seção de projetos e o componente de projeto.

---
+++ src/components/Projeto/index.tsx
@@ -0,0 +1,18 @@
+import Titulo from "../Titulo";
+import Paragrafo from '../Paragrafo';
+
+import {Card, LinkBotao} from './styles'
+
+
+const Projeto = () => {
+    return (
+        <Card>
+            <Titulo>Projeto Lista de Tarefas</Titulo>
+            <Paragrafo tipo="secundario">Lista de Tarefas feita com VueJS</Paragrafo>
+            <LinkBotao>Visulalizar</LinkBotao>
+        </Card>
+    )
+
+}
+
+export default Projeto
--- src/containers/Sobre/styles.ts
+++ src/containers/Sobre/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const GithubSecao = styled.div`
     margin-top: 32px;
-    margin-bottom: 64px;
+    margin-bottom: 34px;
     
     img {
         height: 205px;
