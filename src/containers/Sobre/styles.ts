58a0604bc3a2e05226f65ecbbb9e0ddac663d4a4
Tunae <tunae.santos82@gmail.com>
2024-08-21 10:35:27 -0300

    Finalizando projeto com função de temas

--- src/containers/Sobre/styles.ts
+++ src/containers/Sobre/styles.ts
@@ -6,7 +6,11 @@ export const GithubSecao = styled.div`
     
     img {
         height: 205px;
-        border-radius: 12px;
+
+        @media (max-width: 768px){
+            height: auto;
+            width: 100%;
+        }
     }
 
     
--- src/themes/ligth.ts
+++ src/themes/ligth.ts
@@ -1,5 +1,9 @@
 const temaLight = {
-    corPrincipal: '#000'
+    corPrincipal: '#282a35',
+    corSecundaria: '#949494',
+    corDeFundo: '#fff',
+    corDeFundoBotao: '#4476bf',
+    corDaBorda: '#c1c1c1'
 }
 
 export default temaLight
